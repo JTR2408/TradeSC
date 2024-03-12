084ade4460c11aedc005335135fb3aa14d3a650f
JTR2408 <jade.reed2408@gmail.com>
2024-03-12 02:16:38 +0000

    removed old code

--- src/app/profit.service.ts
+++ src/app/profit.service.ts
@@ -11,39 +11,6 @@ export class ProfitService {
 
   constructor(private dataService: DataService) { }
 
-  // findBestTrade(ship: Ship, investment: number): Observable<string> {
-  //   return this.dataService.getTradePosts().pipe(
-  //     map((tradePosts: tradePost[]) => {
-  //       let maxProfit = 0;
-  //       let bestTrade = '';
-
-  //       if (!tradePosts || tradePosts.length === 0) {
-  //         return 'No trade posts available';
-  //       }
-
-  //       tradePosts.forEach((buyPost: tradePost) => {
-  //         if (!buyPost.prices) {
-  //           return;
-  //         }
-  //         for (const commodity in buyPost.prices) {
-  //           const buyPrice = buyPost.prices[commodity].price_buy;
-  //           const sellPost = this.findBestSellPost(commodity, tradePosts, buyPost);
-  //           if (sellPost) {
-  //             const sellPrice = sellPost.prices[commodity].price_sell;
-  //             const profit = (sellPrice - buyPrice) * ship.scu;
-  //             if (profit > maxProfit && buyPrice * ship.scu <= investment) {
-  //               maxProfit = profit;
-  //               bestTrade = `Buy ${commodity} from ${buyPost.name} and sell at ${sellPost.name}`;
-  //             }
-  //           }
-  //         }
-  //       });
-
-  //       return bestTrade || 'No profitable trade found';
-  //     })
-  //   );
-  // }
-
   private findBestSellPost(commodity: string, tradePosts: tradePost[], buyPost: tradePost): tradePost | null {
     let maxSellPrice = 0;
     let bestSellPost: tradePost | null = null;
